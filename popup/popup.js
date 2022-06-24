450a06827f37d3d689fd616e109c2bc5c5111b56
Diwash <diwashdahal75@gmail.com>
2022-06-24 12:31:32 +0545

    version 1.0

--- popup/popup.js
+++ popup/popup.js
@@ -1,54 +1,12 @@
-// toggling active state
-let toggle = document.getElementById("toggle");
-toggle.addEventListener("click", () => {
-  active = localStorage.getItem("active");
-  if (!active) {
-    localStorage.setItem("active", true);
-  } else {
-    localStorage.setItem("active", false);
-  }
-});
-
-// for slider
+// update dark mode strength on slider change
 let slider = document.getElementById("slider");
 slider.addEventListener("change", async () => {
-  let strength = slider.value;
-  chrome.storage.sync.set({ strength });
+  chrome.storage.sync.set({ strength: slider.value });
+
+  // inject script
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: invertPage,
+    files: ["scripts/invert.js"],
   });
 });
-
-function invertPage() {
-  active = localStorage.getItem("active");
-  if (!active) {
-    chrome.storage.sync.get("strength", ({ strength }) => {
-      hexStr = parseInt(strength, 10).toString(16);
-
-      // create a dark mode div
-      div = document.getElementById("darkDiv");
-      if (!div) {
-        div = document.createElement("div");
-        div.id = "darkDiv";
-      }
-
-      let css = `
-              position: fixed;
-              pointer-events: none;
-              top: 50px;
-              left: 0;
-              width: 100vw;
-              height: 100vh;
-              background-color: #${hexStr}ffffff;
-              mix-blend-mode: difference;
-              z-index: 1; 
-              `;
-
-      // apply the dark mode to the div and append to the webpage
-      div.setAttribute("style", css);
-      document.body.appendChild(div);
-    });
-  }
-}
--- worker.js
+++ worker.js
@@ -1,34 +1,27 @@
 /*
  * This script is always running in background and don't have access to the curent tab
- * so it's a listener. It trigger event with sendMessage function
+ * so it's a listener. It triggers event with sendMessage and executeScript
  */
 
 console.log("Extension loaded!");
+
+// set strength to max initially
+chrome.storage.sync.set({ strength: 255 });
+
+// tab update listener
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (!changeInfo.status === "complete") {
     return;
   }
-
+  // apply dark mode if viewing PDF
   if (tab.url && tab.url.includes(".pdf")) {
     console.log("Viewing a PDF!");
     if (tabId)
       chrome.scripting.executeScript({
         target: { tabId: tabId },
-        files: ["invert.js"],
+        files: ["scripts/invert.js"],
       });
   }
-  // else if (tab.url && tab.url.includes("anilist.co")) {
-  //     console.log("authentification !");
-  //     const url = new URL(tab.url.replace("#", "?"));
-  //     const urlParams = new URLSearchParams(url.search);
-  //     const token = urlParams.get("access_token");
-  //     if (token) {
-  //         chrome.storage.sync.set({
-  //             "token": token
-  //         }, () => {})
-  //     }
-  // }
-
-  console.log("on resolve");
+  console.log("Tab updated!");
   return;
 });
